0cd09ab03c9e9039bb0efb6edcecd56a510651a3
Siddharth Nair <43378710+siddharth17196@users.noreply.github.com>
2019-05-17 23:26:42 +0530

    Update dist.py

--- dist.py
+++ dist.py
@@ -1,21 +1,8 @@
-from math import sin, cos, sqrt, atan2, radians
+#sampele code for calculating diatance between 2 geolocations
 
-# approximate radius of earth in km
-def distance(l1,l2):
-	R = 6373.0
+from geopy.distance import geodesic
 
-	lat1 = radians(l1[0])
-	lon1 = radians(l1[1])
-	lat2 = radians(l2[0])
-	lon2 = radians(l2[1])
+l1=(28.704118728637695, 77.21355438232422)
+l2=(28.705001, 77.212055)
 
-	dlon = lon2 - lon1
-	dlat = lat2 - lat1
-
-	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-	c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-	distance = R * c
-
-	return distance
-#print("Should be:", 278.546, "km")
+print(geodesic(l1,l2).km)
